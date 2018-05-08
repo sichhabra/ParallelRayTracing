a7eef9e1f55e089cde07cc1ea99cf57426615031
sidsudsbucse506 <sichhabra@cs.stonybrook.edu>
2018-05-08 13:12:43 -0400

    Made more misc parallel.

--- raytrace_serial/raytrace/antialias.h
+++
@@ -1,9 +0,0 @@
-#include <iostream>
-#include "imager.h"
-using namespace std;
-
-namespace Imager{
-
-    Color cuda_antiAlias(double **red,double **green,double **blue,int i,int j,int antiAliasFactor);
-
-}
